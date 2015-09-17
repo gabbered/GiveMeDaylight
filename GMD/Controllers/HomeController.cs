6b602948f4e97d4008d2cd780ee2899be0e0a65d
Gary Bryant <gabbered@hotmail.com>
2015-09-16 19:31:59 -0500

    more clean up

--- GMD/Controllers/HomeController.cs
+++ GMD/Controllers/HomeController.cs
@@ -84,27 +84,6 @@ namespace GMD.Controllers
             }
             return string.Empty;
 
-        }
-
-        public ActionResult Index2()
-        {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        }        
     }
 }
